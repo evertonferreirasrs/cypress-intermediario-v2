a048ad2bab66f06b70a1b4bcbf308e743952b275
Everton Ap. Ferreira <evertonferreira_srs@hotmail.com>
2023-01-31 20:54:36 -0300

    Testando a funcionalidade criação de projeto - 1

--- cypress/e2e/gui/logout.cy.js
+++ cypress/e2e/gui/logout.cy.js
@@ -1,12 +1,12 @@
 describe('Logout', () => {
     beforeEach(() => {
-      cy.login()
-      cy.visit('/')
+        cy.login()
+        cy.visit('/')
     })
-  
+
     it('successfully', () => {
-      cy.logout()
-  
-      cy.url().should('be.equal', `${Cypress.config('baseUrl')}/users/sign_in`)
+        cy.logout()
+
+        cy.url().should('be.equal', `${Cypress.config('baseUrl')}/users/sign_in`)
     })
-  })
+})
