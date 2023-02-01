dd077aacc01832288047f7529318722a78d3d966
Everton Ap. Ferreira <evertonferreira_srs@hotmail.com>
2023-01-31 21:05:06 -0300

    Testando a funcionalidade criação de projeto - 2 sessão

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -1,4 +1,4 @@
-Cypress.Commands.add('login', (user = Cypress.env('user_name'), password = Cypress.env('user_password'),) => {
+Cypress.Commands.add('login', (user = Cypress.env('user_name'), password = Cypress.env('user_password'), { cacheSession = true } = {},) => {
     const login = () => {
         cy.visit('/users/sign_in')
 
@@ -7,7 +7,15 @@ Cypress.Commands.add('login', (user = Cypress.env('user_name'), password = Cypre
         cy.get("[data-qa-selector='sign_in_button']").click()
     }
 
-    login()
+    const options = {
+        cacheAcrossSpecs: true,
+    }
+
+    if (cacheSession) {
+        cy.session(user, login, options)
+    } else {
+        login()
+    }
 })
 
 Cypress.Commands.add('logout', () => {
