cb8fa88c0a83eea201ae03d4635d4cf10d15d874
Everton Ap. Ferreira <evertonferreira_srs@hotmail.com>
2023-01-31 21:40:18 -0300

    Testando criação de projeto via API

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -14,8 +14,9 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+
 import './gui_commands'
+import './api_commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
